7ad61864be27cad3c342e16cc1fd2f6955e72c86
Mark-Boon <mnboon@gmail.com>
2018-12-19 11:28:06 +0100

    added benchmark scripts

--- tests/games/test_uttt_network.py
+++ tests/games/test_uttt_network.py
@@ -1,13 +1,16 @@
 import unittest as test
 from pprint import pprint
 
-
 import functools
 import games.uttt as ut
 import tensorflow as tf
 import time
+import json
+import os
+import re
 
-from common.network_helpers import load_network, get_stochastic_network_move, create_network
+import common.custom_cnn as cnn
+from common.network_helpers import load_network, get_stochastic_network_move, create_network, get_deterministic_network_move
 from common.visualisation import load_results
 
 class TestUltimateTicTacToe(test.TestCase):
@@ -16,8 +19,45 @@ class TestUltimateTicTacToe(test.TestCase):
 	# run with: py -m unittest tests\games\test_uttt_network.py
 	def test_game_network(self):
 
-		network_file_path = 'networks/regularization_51_e-3b100/net_ep54000.p'
-		n = 1000
+		network_file_path = r'C:\Users\User\APH\1B 2017 2018\Advanced Machine Learning\Resit\Git\QLUT\networks\cnn_10_10_10_e-3_det_1e-1\\'
+		n = 100
+		steps_games = 5
+		cnn_on = True
+		filter_shape = [3, 3]
+		filter_depth = [10, 10, 10]
+		dense_width = []
+		input_layer = 90
+		hidden_layers = [51, 51, 51]
+		output_layer = 81
+
+		f = []
+		for (dirpath, dirnames, filenames) in os.walk(network_file_path):
+			f.extend(filenames)
+			break
+
+		netlist_hist = []
+		raw_netlist = []
+		for file in f:
+			p = re.compile('net_ep\d+_.+\.p')
+			if 'config' in file:
+				pass
+			elif 'hist' in file:
+				historic_net = file
+			elif p.search(file) is None:
+				netlist_hist.append(file)
+			else:
+				raw_netlist.append(file)
+
+		nr_games = []
+		for i, name in enumerate(raw_netlist):
+			nr_games.append((int(name[6:-20]), i))
+		nr_games.sort()
+
+		netlist = [raw_netlist[i[1]] for i in nr_games]
+		gamefiles = [ netlist[(i+1)*steps_games - 1] for i in range(0,int(len(netlist)/steps_games))]
+		network_games = [nr_games[(i+1)*steps_games - 1][0] for i in range(0,int(len(netlist)/steps_games))]
+		print(gamefiles)
+		print(gamefiles[0][1])
 
 		# if resultfile exists:
 		# network_info = load_results(network_file_path, results_only = False)
@@ -25,9 +65,8 @@ class TestUltimateTicTacToe(test.TestCase):
 		# hidden_layers = network_info['hidden_layers']
 		# output_layer = network_info['output_layer']
 		# otherwise look up values
-		input_layer = 90
-		hidden_layers = [51]
-		output_layer = 81
+
+
 
 		game_spec = ut.UltimateTicTacToeGameSpec()
 
@@ -35,30 +74,52 @@ class TestUltimateTicTacToe(test.TestCase):
 		# opponent_func = game_spec.get_manual_player_func()
 
 		def player_func(board_state, side):
-			move = get_stochastic_network_move(session, input_layer, output_layer, board_state, side, valid_only = True, game_spec = game_spec)
+			move = get_deterministic_network_move(session, input_layer, output_layer, board_state, side, valid_only = True, game_spec = game_spec)
 			return game_spec.flat_move_to_tuple(move.argmax())
 
-		create_network_func = functools.partial(create_network, input_layer, hidden_layers, output_layer)
+		if cnn_on:
+			create_network_func = functools.partial(cnn.create_network,
+											   filter_shape,
+											   filter_depth,
+											   dense_width)
+		else:
+			create_network_func = functools.partial(create_network, input_layer, hidden_layers, output_layer)
+
+
 		input_layer, output_layer, variables, _ = create_network_func()
+		results = {}
+		for i in range(len(gamefiles)):
+			t = time.perf_counter()
+			with tf.Session() as session:
+				session.run(tf.global_variables_initializer())
+				print("loading pre-existing network")
+				load_network(session, variables, network_file_path + gamefiles[i]) #\\
 
-		t = time.perf_counter()	
-		with tf.Session() as session:
-			session.run(tf.global_variables_initializer())
-			print("loading pre-existing network")
-			load_network(session, variables, network_file_path)
+				results_X = [game_spec.play_game(player_func, opponent_func) for _ in range(int(n/2))]
+				results_O = [game_spec.play_game(opponent_func, player_func) for _ in range(int(n/2))]
+				elapsed_time = time.perf_counter() - t
 
-			results_X = [game_spec.play_game(player_func, opponent_func) for i in range(int(n/2))]
-			results_O = [game_spec.play_game(opponent_func, player_func) for i in range(int(n/2))]
-			elapsed_time = time.perf_counter() - t
-			print('Elapsed time:', elapsed_time)
-			print('Network as X (%s games):' % (n/2))
-			print('Player X wins: ', results_X.count(1)/n*2)
-			print('Player O wins: ', results_X.count(-1)/n*2)
-			print('Draws        : ', results_X.count(0)/n*2)
-			print('Winrate X    : ', 0.5 + 1.*sum(results_X)/n*2)
+				results[network_games[i]] = results_X + [-j for j in results_O]
+				print('network ' + gamefiles[i])
+				"""
+				print('Elapsed time:', elapsed_time)
+				print('Network as X (%s games):' % (n/2))
+				print('Player X wins: ', results_X.count(1)/n*2)
+				print('Player O wins: ', results_X.count(-1)/n*2)
+				print('Draws        : ', results_X.count(0)/n*2)
+				print('Winrate X    : ', 0.5 + 1.*sum(results_X)/n*2)
 
-			print('Network as O (%s games):' % (n/2))
-			print('Player O wins: ', results_O.count(-1)/n*2)
-			print('Player X wins: ', results_O.count(1)/n*2)
-			print('Draws        : ', results_O.count(0)/n*2)
-			print('Winrate O    : ', 0.5 - 1.*sum(results_O)/n*2)
+				print('Network as O (%s games):' % (n/2))
+				print('Player O wins: ', results_O.count(-1)/n*2)
+				print('Player X wins: ', results_O.count(1)/n*2)
+				print('Draws        : ', results_O.count(0)/n*2)
+				print('Winrate O    : ', 0.5 - 1.*sum(results_O)/n*2)
+
+				"""
+
+		with open(network_file_path +'_benchmark_vs_rand.json', 'w') as outfile:
+			json.dump(results, outfile)
+
+
+if __name__ == '__main__':
+	test.main()
